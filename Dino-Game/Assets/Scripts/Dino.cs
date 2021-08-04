0420f2f2e8fdfaf5a793400ff522f8889cb05a89
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-08-04 12:27:26 +0200

    fix jump

--- Dino-Game/Assets/Scripts/Dino.cs
+++ Dino-Game/Assets/Scripts/Dino.cs
@@ -4,31 +4,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Dino : MonoBehaviour {
-    
     [SerializeField] private float jumpForce; // 700 at Gravity Scale 2
-    private Rigidbody2D rb;
-    private bool jump = false;
-    
+    private Rigidbody2D _rb;
+    private BoxCollider2D _collider2D;
+    [SerializeField] private bool canJump = true;
+
     // Start is called before the first frame update
     void Start() {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _collider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
-            Debug.Log("Update Jump");
+        CheckJump();
+
+        if (canJump && Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) {
             Jump();
         }
+    }
 
+    private void CheckJump() {
+        var colliders = new List<Collider2D>();
+        if (_collider2D.GetContacts(colliders) >= 1 && colliders[0].CompareTag("Ground")) {
+                canJump = true;
+        }
+        else {
+            canJump = false;
+        }
     }
 
     public void Jump() {
-        rb.AddForce(Vector2.up * jumpForce);
-    }
-
-    private void FixedUpdate() {
-        
+        canJump = false;
+        _rb.AddForce(Vector2.up * jumpForce);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
