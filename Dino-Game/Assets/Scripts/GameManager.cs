a8cca428b4c6b247029f0a0f20a8e9c479450828
Fabian Weber <faweber@stud.hs-heilbronn.de>
2021-08-10 12:14:55 +0200

    Removed temp Dino Game

--- Dino-Game/Assets/Scripts/GameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class GameManager : MonoBehaviour {
-    [SerializeField] public Counter counter;
-
-    public void ResetGame() {
-        counter.ResetCounter();
-    }
-}
