e01306bf9c32f16e1da82fe33d13856645018f73
Fabian Weber <fabiandotweber@gmail.com>
2021-08-13 12:07:21 +0200

    Add Training Sample

--- Projekts3D/StartTraining.bat
+++ Projekts3D/StartTraining.bat
@@ -1,6 +1,6 @@
 @echo on
 call conda activate ml-agents
-mlagents-learn --force
+mlagents-learn Config/config.yaml --force --torch-device cuda --env=Build/Projekts3D.exe
 call conda deactivate
 :: @pause
 cmd /k
